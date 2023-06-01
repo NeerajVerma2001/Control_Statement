7ee2c6f5b96c8a017b645732a7bc1ae2968f851e
Neeraj Verma <113288731+NeerajVerma2001@users.noreply.github.com>
2023-06-01 23:33:41 +0530

    added Swapping.java

---
+++ Swapping1.java
@@ -0,0 +1,14 @@
+
+public class Swapping1 
+{
+	public static void main(String[] args) 
+	{
+		// swapping values by using two numbers
+		int a=20,b=30;
+		a=a+b;
+		b=a-b;
+		a=a-b;
+		System.out.println("value of a : "+a);
+		System.out.println("value of b:"+ b);
+	}
+}
