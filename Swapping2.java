c5d9d00bfd1771512a523df244c86aa75617cf22
Neeraj Verma <113288731+NeerajVerma2001@users.noreply.github.com>
2023-06-01 23:38:16 +0530

    added Swapping2.java

---
+++ Swapping2.java
@@ -0,0 +1,13 @@
+
+public class Swapping2 
+{
+	public static void main(String[] args) {
+		//swapping by 3 numbers by using temp
+		 int a=45,b=23,temp;
+		 temp=a;
+		 a=b;
+		 b=temp;
+		 System.out.println("a="+a);
+		 System.out.println("b:"+b);
+	}
+}
